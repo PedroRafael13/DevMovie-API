bfb45511bd202cd0f6aae49aa2ec98374d12b6d9
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-06-14 23:30:01 -0400

    ..

--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,30 @@
-class APpp {
-  constructor(){
-    
-  }
+module.exports = {
+  dialect : "postgres",
+  host : "localhost",
+  username : "postgres",
+  password : "postgres",
+  database : "devburger",
+  define : {
+    timestamps : true,
+    underscored : true,
+    underscoredAll: true,
+  },
 }
+
+import express from 'express'
+import routes from'./routes'
+class App {
+  constructor(){
+    this.app = express()
+  }
+
+  middleware(){
+    this.app.use(express.json())
+  }
+
+  routes(){
+    this.app.use(routes())
+}
+}
+
+export default new App().app
